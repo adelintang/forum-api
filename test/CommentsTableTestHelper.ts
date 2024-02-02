38da052aab715eb3fca43294dd8d183fc0971eb8
adelintang <adelintang157@gmail.com>
2024-02-02 22:15:57 +0700

    add: all tests

---
+++ test/CommentsTableTestHelper.ts
@@ -0,0 +1,40 @@
+/* istanbul ignore file */
+import pool from '../src/Infrastructures/database/postgres/pool'
+
+const CommentsTableTestHelper = {
+  async addComment ({
+    id = 'comment-123', owner = 'user-123', threadId = 'thread-123', content = 'Lorem ipsum sit dolor', date = new Date().toISOString, isDelete = 'false'
+  }) {
+    const query = {
+      text: 'INSERT INTO comments VALUES($1, $2, $3, $4, $5, $6)',
+      values: [id, threadId, owner, content, date, isDelete]
+    }
+
+    await pool.query(query)
+  },
+
+  async addDeleteComment ({
+    id = 'comment-123', owner = 'user-123', threadId = 'thread-123', content = 'Lorem ipsum sit dolor', date = new Date().toISOString
+  }) {
+    await this.addComment({
+      id, owner, threadId, content, date, isDelete: 'true'
+    })
+  },
+
+  async findCommentById (id: string) {
+    const query = {
+      text: 'SELECT * FROM comments WHERE id = $1',
+      values: [id]
+    }
+
+    const result = await pool.query(query)
+
+    return result.rows
+  },
+
+  async cleanTable () {
+    await pool.query('DELETE FROM comments WHERE 1=1')
+  }
+}
+
+export default CommentsTableTestHelper
